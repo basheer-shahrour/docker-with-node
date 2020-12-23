c79bf76d1be1cf770a5330779409f5fee71ffc68
Basheer Shahrour <shahrourbasheer1997@gmail.com>
2020-12-23 22:28:45 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.get("/", (req, res) => res.send("Hello from Basheer's docker container!!"));
+
+const used = process.memoryUsage().heapUsed / 1024 / 1024;
+console.log(`The script uses approximately ${used} MB`);
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
