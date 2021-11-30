cd82520facc10650848454dc802a34eab0dc487e
Basheer Shahrour <shahrourbasheer1997@gmail.com>
2021-11-30 23:49:58 +0200

    test watchtower

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
-app.get("/", (req, res) => res.send("Hello from Basheer's docker container!!"));
+app.get("/", (req, res) =>
+  res.send("Hello from Basheer's docker container!! + test watchtower")
+);
 
 const used = process.memoryUsage().heapUsed / 1024 / 1024;
 console.log(`The script uses approximately ${used} MB !!!!!!!!!!!!!!!`);
