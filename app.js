0a09b760684f2a93d4ec8c3b4d4a8a92bb53f692
Basheer Shahrour <shahrourbasheer1997@gmail.com>
2021-12-01 11:47:35 +0200

    swarm setup

--- app.js
+++ app.js
@@ -11,3 +11,9 @@ const used = process.memoryUsage().heapUsed / 1024 / 1024;
 console.log(`The script uses approximately ${used} MB !!!!!!!!!!!!!!!`);
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+
+// docker run -d \
+// > --name watchtower \
+// > -e WATCHTOWER_TRACE=true -e WATCHTOWER_DEBUG=true -e WATCHTOWER_POLL_INTERVAL=50 \
+// > -v /var/run/docker.sock:/var/run/docker.sock \
+// > containrrr/watchtower <image-name>
